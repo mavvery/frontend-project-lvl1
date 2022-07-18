4ee44edd963489b5814353e050128b54aecd2eca
Katya Vinokurova <vynokurova.katya2001@gmail.com>
2022-07-18 18:51:13 +0400

    add all

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import startGameEngine from '../src/games/calc.js';
+
+startGameEngine();
---
+++ src/games/calc.js
@@ -0,0 +1,25 @@
+import getRandomNumber from '../utils.js';
+import startGameEngine from '../index.js';
+
+const RulesGames = 'What is the result of the expression?';
+
+const calc = (num1, num2, operator) => {
+  if (operator === '+') {
+    return num1 + num2;
+  } if (operator === '-') {
+    return num1 - num2;
+  } return num1 * num2;
+};
+
+const getQuestionCorrectAnswer = () => {
+  const operators = ['+', '-', '*'];
+  const number1 = getRandomNumber(1, 100);
+  const number2 = getRandomNumber(1, 100);
+  const operator = operators[getRandomNumber(0, 3)];
+
+  const question = `${number1} ${operator} ${number2}`;
+  let CorrectAnswer = String(calc(number1, number2, operator));
+
+  return [question, CorrectAnswer];
+};
+export default () => startGameEngine(RulesGames, getQuestionCorrectAnswer);
