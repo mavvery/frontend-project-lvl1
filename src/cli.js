cb3e85dd71382329b520e97e29688d33c69c8e16
Katya Vinokurova <vynokurova.katya2001@gmail.com>
2022-07-25 17:13:45 +0400

    add all

--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
-export default userName;
+export { userName as default };
